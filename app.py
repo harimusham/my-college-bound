b30e74d9d7665d633e9a7109f16ff2e1f5c4d57c
harimusham <121648601+harimusham@users.noreply.github.com>
2023-08-30 14:23:56 +0000

    updated code

--- app.py
+++ app.py
@@ -1,7 +1,38 @@
-from flask import Flask ,render_template
+from flask import Flask ,render_template,jsonify
 app=Flask(__name__)
+JOBS=[
+  {
+    'id':1,
+    'title':'froent-end engineer',
+    'location':'hyderabad',
+    'salary':120000
+  },
+  {
+    'id':2,
+    'title':'backend developer',
+    'location':'benguluru',
+    'salary':900000
+  },
+  {
+   'id':3,
+    'title':'java developer',
+    'location':'kattangur',
+    'salary':904533
+  },
+  {
+   'id':4,
+    'title':'html developer',
+    'location':'nakrekal',
+    'salary':999999
+  }
+  
+]
 @app.route("/")
-def hello_world():
-  return render_template('home.html')
+def hello_harikiran():
+  return render_template('home.html',jobs=JOBS,company_name="harikiran company")
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
